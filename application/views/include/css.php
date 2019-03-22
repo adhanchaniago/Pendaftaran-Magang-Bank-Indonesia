26396f98814a9185d11c2e0f571b8ee1038c9d5b
iniwahyu <wrep17@gmail.com>
2019-03-22 17:26:35 +0700

    Fitur Export Excel

--- application/views/include/css.php
+++ application/views/include/css.php
@@ -10,6 +10,8 @@
 <link rel="stylesheet" href="<?php echo base_url('asset/'); ?>bower_components/Ionicons/css/ionicons.min.css">
 <!-- DataTables -->
 <link rel="stylesheet" href="<?php echo base_url('asset/'); ?>bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+<!-- <link rel="stylesheet" href="<?php echo base_url('asset/custom/css/jquery.dataTables.min.css'); ?>"> -->
+<link rel="stylesheet" href="<?php echo base_url('asset/custom/css/buttons.dataTables.min.css'); ?>">
 <!-- Theme style -->
 <link rel="stylesheet" href="<?php echo base_url('asset/'); ?>dist/css/AdminLTE.min.css">
 <link rel="stylesheet" href="<?php echo base_url('asset/'); ?>dist/css/skins/_all-skins.min.css">
--- application/views/include/js.php
+++ application/views/include/js.php
@@ -2,28 +2,23 @@
 <script src="<?php echo base_url('asset/'); ?>bower_components/jquery/dist/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="<?php echo base_url('asset/'); ?>bower_components/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
 <!-- Bootstrap 3.3.7 -->
 <script src="<?php echo base_url('asset/'); ?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- DataTables -->
 <script src="<?php echo base_url('asset/'); ?>bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="<?php echo base_url('asset/'); ?>bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="<?php echo base_url('asset/custom/js/dataTables.buttons.min.js') ?>"></script>
+<script src="<?php echo base_url('asset/custom/js/buttons.flash.min.js') ?>"></script>
+<script src="<?php echo base_url('asset/custom/js/jszip.min.js') ?>"></script>
+<script src="<?php echo base_url('asset/custom/js/pdfmake.min.js') ?>"></script>
+<script src="<?php echo base_url('asset/custom/js/buttons.html5.min.js') ?>"></script>
+<script src="<?php echo base_url('asset/custom/js/buttons.print.min.js') ?>"></script>
 <!-- Sparkline -->
 <script src="<?php echo base_url('asset/'); ?>bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
 <!-- jQuery Knob Chart -->
 <script src="<?php echo base_url('asset/'); ?>bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
 <!-- Select2 -->
 <script src="<?php echo base_url('asset/'); ?>bower_components/select2/dist/js/select2.full.min.js"></script>
-<!-- daterangepicker -->
-<script src="<?php echo base_url('asset/'); ?>bower_components/moment/min/moment.min.js"></script>
-<script src="<?php echo base_url('asset/'); ?>bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="<?php echo base_url('asset/'); ?>bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="<?php echo base_url('asset/'); ?>plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 <!-- Slimscroll -->
 <script src="<?php echo base_url('asset/'); ?>bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
@@ -35,8 +30,15 @@
 <!-- AdminLTE for demo purposes -->
 <script src="<?php echo base_url('asset/'); ?>dist/js/demo.js"></script>
 <script>
+
 $(function () {
-	$('#example1').DataTable()
+	$('#example1').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+    // 'copy', 'csv', 'excel', 'pdf', 'print'
+    'excel'
+        ]
+  })
 	$('.select2').select2()
 })
 </script>
